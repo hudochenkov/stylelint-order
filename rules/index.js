8d6a041fa6ac53ae5cf0c27072a9a4ae36abf88d
Aleks Hudochenkov <aleks@hudochenkov.com>
2016-11-04 23:10:06 +0300

    Fix unavailability of declaration-block-properties-alphabetical-order rule
    ¯﻿\﻿_﻿(﻿ツ﻿)﻿_﻿/﻿¯

--- rules/index.js
+++ rules/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+	'declaration-block-properties-alphabetical-order': require('./declaration-block-properties-alphabetical-order'),
 	'declaration-block-order': require('./declaration-block-order'),
 	'property-groups-structure': require('./property-groups-structure'),
 };
