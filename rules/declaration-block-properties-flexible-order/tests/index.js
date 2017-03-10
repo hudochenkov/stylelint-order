d651a30970131d7b88ac7356651c4c8099cb10a8
Cas Cornelissen <cas.cornelissen@freshheads.com>
2017-03-10 09:40:40 +0100

    Switches tests to stylelint's orginal tests

--- rules/declaration-block-properties-flexible-order/tests/index.js
+++ rules/declaration-block-properties-flexible-order/tests/index.js
@@ -10,32 +10,120 @@ testRule(rule, {
 	ruleName,
 
 	config: [[
-		'position',
+		'height',
+		'width',
 		{
 			order: 'flexible',
-			properties: ['top', 'right', 'bottom', 'left'],
+			properties: ['color', 'font-size', 'font-weight'],
 		},
-		'margin',
-		'padding',
 	]],
 
 	accept: [
 		{
-			code: '.foo { position: absolute; margin: 0; }',
+			code: 'a { height: 1px; width: 2px; color: pink; font-size: 2px; font-weight: bold; }',
 		},
 		{
-			code: '.foo { position: absolute; right: 0; top: 0; padding: 0; }',
+			code: 'a { height: 1px; width: 2px; font-size: 2px; color: pink; font-weight: bold; }',
+		},
+		{
+			code: 'a { height: 1px; width: 2px; font-size: 2px; font-weight: bold; color: pink; }',
+		},
+		{
+			code: 'a { height: 1px; width: 2px; font-weight: bold; font-size: 2px; color: pink; }',
+		},
+		{
+			code: 'a { height: 10px; background: orange; }',
+			description: 'unspecified after groupless specified',
+		},
+		{
+			code: 'a { font-weight: bold; background: orange; }',
+			description: 'unspecified after grouped specified',
+		},
+		{
+			code: 'a { background: orange; height: 10px; }',
+			description: 'unspecified before groupless specified',
+		},
+		{
+			code: 'a { background: orange; font-weight: bold; }',
+			description: 'unspecified before grouped specified',
 		},
 	],
 
 	reject: [
 		{
-			code: '.foo { position: absolute; margin: 0; top: 0; }',
-			message: messages.expected('top', 'margin'),
+			code: 'a { height: 1px; font-weight: bold; width: 2px; }',
+			message: messages.expected('width', 'font-weight'),
+			line: 1,
+			column: 37,
 		},
 		{
-			code: '.foo { position: absolute; left: 0; margin: 0; top: 0; }',
-			message: messages.expected('top', 'margin'),
+			code: 'a { font-weight: bold; height: 1px; width: 2px; }',
+			message: messages.expected('height', 'font-weight'),
+			line: 1,
+			column: 24,
+		},
+		{
+			code: 'a { width: 2px; height: 1px; font-weight: bold; }',
+			message: messages.expected('height', 'width'),
+			line: 1,
+			column: 17,
+		},
+		{
+			code: 'a { height: 1px; color: pink; width: 2px; font-weight: bold; }',
+			message: messages.expected('width', 'color'),
+			line: 1,
+			column: 31,
+		},
+	],
+});
+
+testRule(rule, {
+	ruleName,
+
+	config: [[
+		{
+			order: 'flexible',
+			properties: ['width', 'height'],
+		},
+		{
+			order: 'flexible',
+			properties: ['color', 'font-size', 'font-weight'],
+		},
+	]],
+
+	accept: [
+		{
+			code: 'a { height: 1px; width: 2px; color: pink; font-size: 2px; font-weight: bold; }',
+		},
+		{
+			code: 'a { width: 2px; height: 1px; font-size: 2px; color: pink; font-weight: bold; }',
+		},
+		{
+			code: 'a { height: 1px; width: 2px; font-size: 2px; font-weight: bold; color: pink; }',
+		},
+		{
+			code: 'a { width: 2px; height: 1px; font-weight: bold; font-size: 2px; color: pink; }',
+		},
+	],
+
+	reject: [
+		{
+			code: 'a { height: 1px; font-weight: bold; width: 2px; }',
+			message: messages.expected('width', 'font-weight'),
+			line: 1,
+			column: 37,
+		},
+		{
+			code: 'a { font-weight: bold; height: 1px; width: 2px; }',
+			message: messages.expected('height', 'font-weight'),
+			line: 1,
+			column: 24,
+		},
+		{
+			code: 'a { height: 1px; color: pink; width: 2px; font-weight: bold; }',
+			message: messages.expected('width', 'color'),
+			line: 1,
+			column: 31,
 		},
 	],
 });
