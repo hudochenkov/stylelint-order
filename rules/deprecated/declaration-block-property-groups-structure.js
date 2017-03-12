e1eb4115e686caa73ddb28ea1fb71519fd30b2b6
Aleks Hudochenkov <aleks@hudochenkov.com>
2017-03-12 22:06:25 +0300

    Remove rules included in `properties-order`
    * declaration-block-properties-flexible-order
    * declaration-block-properties-specified-order
    * declaration-block-property-groups-structure

---
+++ rules/deprecated/declaration-block-property-groups-structure.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const utils = require('../../utils');
+
+const ruleName = utils.namespace('declaration-block-property-groups-structure');
+
+function rule() {
+	return function (root, result) {
+		result.warn(
+			(
+				`"${ruleName}" has been removed in 0.4. Instead use "${utils.namespace('declaration-block-properties-order')}" rule.`
+			),
+			{
+				stylelintType: 'deprecation',
+			}
+		);
+	};
+}
+
+rule.primaryOptionArray = true;
+
+module.exports = rule;
+module.exports.ruleName = ruleName;
--- rules/index.js
+++ rules/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-	'declaration-block-properties-specified-order': require('./declaration-block-properties-specified-order'),
-	'declaration-block-properties-alphabetical-order': require('./declaration-block-properties-alphabetical-order'),
-	'declaration-block-properties-flexible-order': require('./declaration-block-properties-flexible-order'),
 	'declaration-block-order': require('./declaration-block-order'),
-	'declaration-block-property-groups-structure': require('./declaration-block-property-groups-structure'),
 	'declaration-block-properties-order': require('./declaration-block-properties-order'),
+	'declaration-block-properties-alphabetical-order': require('./declaration-block-properties-alphabetical-order'),
+
+	'declaration-block-properties-specified-order': require('./deprecated/declaration-block-properties-specified-order'),
+	'declaration-block-property-groups-structure': require('./deprecated/declaration-block-property-groups-structure'),
 };
