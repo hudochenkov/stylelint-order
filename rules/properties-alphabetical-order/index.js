f5e0058ce31db2c7577d4d5d483fbe91cab435c8
Aleks Hudochenkov <aleks@hudochenkov.com>
2017-05-28 22:17:36 +0300

    Restructure `order` code

--- rules/properties-alphabetical-order/index.js
+++ rules/properties-alphabetical-order/index.js
@@ -29,74 +29,74 @@ function rule(actual) {
 		// Check all rules and at-rules recursively
 		root.walk(function processRulesAndAtrules(node) {
 			if (node.type === 'rule' || node.type === 'atrule') {
-				checkNode(node);
+				checkNode(node, result);
 			}
 		});
-
-		function checkNode(node) {
-			// Skip if it's an empty rule
-			if (!node.nodes || !node.nodes.length) {
-				return;
-			}
-
-			const allPropData = [];
-
-			node.each(function processEveryNode(child) {
-				if (child.type !== 'decl') {
-					return;
-				}
-
-				const prop = child.prop;
-
-				if (!utils.isStandardSyntaxProperty(prop)) {
-					return;
-				}
-
-				if (utils.isCustomProperty(prop)) {
-					return;
-				}
-
-				let unprefixedPropName = postcss.vendor.unprefixed(prop);
-
-				// Hack to allow -moz-osx-font-smoothing to be understood
-				// just like -webkit-font-smoothing
-				if (unprefixedPropName.indexOf('osx-') === 0) {
-					unprefixedPropName = unprefixedPropName.slice(4);
-				}
-
-				const propData = {
-					name: prop,
-					unprefixedName: unprefixedPropName,
-					index: allPropData.length,
-					node: child,
-				};
-
-				const previousPropData = _.last(allPropData);
-
-				allPropData.push(propData);
-
-				// Skip first decl
-				if (!previousPropData) {
-					return;
-				}
-
-				const isCorrectOrder = checkAlphabeticalOrder(previousPropData, propData);
-
-				if (isCorrectOrder) {
-					return;
-				}
-
-				stylelint.utils.report({
-					message: messages.expected(propData.name, previousPropData.name),
-					node: child,
-					result,
-					ruleName,
-				});
-			});
-		}
 	};
 }
 
 module.exports = rule;
 module.exports.ruleName = ruleName;
 module.exports.messages = messages;
+
+function checkNode(node, result) {
+	// Skip if it's an empty rule
+	if (!node.nodes || !node.nodes.length) {
+		return;
+	}
+
+	const allPropData = [];
+
+	node.each(function processEveryNode(child) {
+		if (child.type !== 'decl') {
+			return;
+		}
+
+		const prop = child.prop;
+
+		if (!utils.isStandardSyntaxProperty(prop)) {
+			return;
+		}
+
+		if (utils.isCustomProperty(prop)) {
+			return;
+		}
+
+		let unprefixedPropName = postcss.vendor.unprefixed(prop);
+
+		// Hack to allow -moz-osx-font-smoothing to be understood
+		// just like -webkit-font-smoothing
+		if (unprefixedPropName.indexOf('osx-') === 0) {
+			unprefixedPropName = unprefixedPropName.slice(4);
+		}
+
+		const propData = {
+			name: prop,
+			unprefixedName: unprefixedPropName,
+			index: allPropData.length,
+			node: child,
+		};
+
+		const previousPropData = _.last(allPropData);
+
+		allPropData.push(propData);
+
+		// Skip first decl
+		if (!previousPropData) {
+			return;
+		}
+
+		const isCorrectOrder = checkAlphabeticalOrder(previousPropData, propData);
+
+		if (isCorrectOrder) {
+			return;
+		}
+
+		stylelint.utils.report({
+			message: messages.expected(propData.name, previousPropData.name),
+			node: child,
+			result,
+			ruleName,
+		});
+	});
+}
