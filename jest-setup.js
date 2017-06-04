030fc4856e01c35b9571c96e33a824cc12e93644
Aleks Hudochenkov <aleks@hudochenkov.com>
2017-06-04 15:34:23 +0300

    Update jest-setup to match stylelint's one

--- jest-setup.js
+++ jest-setup.js
@@ -4,6 +4,23 @@ const _ = require('lodash');
 const stylelint = require('stylelint');
 
 global.testRule = (rule, schema) => {
+	expect.extend({
+		toHaveMessage(testCase) {
+			if (testCase.message === undefined) {
+				return {
+					message: () => (
+						'Expected "reject" test case to have a "message" property'
+					),
+					pass: false,
+				};
+			}
+
+			return {
+				pass: true,
+			};
+		},
+	});
+
 	describe(schema.ruleName, () => {
 		const stylelintConfig = {
 			plugins: [
@@ -20,12 +37,24 @@ global.testRule = (rule, schema) => {
 					const spec = (testCase.only) ? it.only : it;
 
 					spec(testCase.description || 'no description', () => {
-						return stylelint.lint({
+						const options = {
 							code: testCase.code,
 							config: stylelintConfig,
 							syntax: schema.syntax,
-						}).then((output) => {
+						};
+
+						return stylelint.lint(options).then((output) => {
 							expect(output.results[0].warnings).toEqual([]);
+							if (!schema.fix) {
+								return;
+							}
+
+							// Check the fix
+							return stylelint.lint(Object.assign({ fix: true }, options)).then((output2) => {
+								const fixedCode = getOutputCss(output2);
+
+								expect(fixedCode).toBe(testCase.code);
+							});
 						});
 					});
 				});
@@ -38,25 +67,45 @@ global.testRule = (rule, schema) => {
 					const spec = (testCase.only) ? it.only : it;
 
 					spec(testCase.description || 'no description', () => {
-						return stylelint.lint({
+						const options = {
 							code: testCase.code,
 							config: stylelintConfig,
 							syntax: schema.syntax,
-						}).then((output) => {
+						};
+
+						return stylelint.lint(options).then((output) => {
 							const warnings = output.results[0].warnings;
 							const warning = warnings[0];
 
 							expect(warnings.length).toBeGreaterThanOrEqual(1);
+							// expect(testCase).toHaveMessage();
 
 							if (testCase.message !== undefined) {
 								expect(_.get(warning, 'text')).toBe(testCase.message);
 							}
+
 							if (testCase.line !== undefined) {
 								expect(_.get(warning, 'line')).toBe(testCase.line);
 							}
+
 							if (testCase.column !== undefined) {
 								expect(_.get(warning, 'column')).toBe(testCase.column);
 							}
+
+							if (!schema.fix) {
+								return;
+							}
+
+							if (!testCase.fixed) {
+								throw new Error('If using { fix: true } in test schema, all reject cases must have { fixed: .. }');
+							}
+
+							// Check the fix
+							return stylelint.lint(Object.assign({ fix: true }, options)).then((output2) => {
+								const fixedCode = getOutputCss(output2);
+
+								expect(fixedCode).toBe(testCase.fixed);
+							});
 						});
 					});
 				});
@@ -64,6 +113,13 @@ global.testRule = (rule, schema) => {
 		}
 	});
 };
+
+function getOutputCss(output) {
+	const result = output.results[0]._postcssResult;
+	const css = result.root.toString(result.opts.syntax);
+
+	return css;
+}
 
 global.testConfig = (input) => {
 	let testFn;
