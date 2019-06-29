5c03c7318cf1581cc17f457d7b96f223e84fe04e
Aleks Hudochenkov <aleks@hudochenkov.com>
2019-06-29 23:25:51 +0200

    Report warnings, if autofix didn't fix them for properties-order

--- rules/properties-order/checkNode.js
+++ rules/properties-order/checkNode.js
@@ -8,77 +8,65 @@ const getNodeData = require('./getNodeData');
 const createFlatOrder = require('./createFlatOrder');
 
 module.exports = function checkNode(node, sharedInfo, originalNode) {
-	let shouldFixOrder = false;
-
-	const allPropData = node.nodes
-		.filter(item => utils.isProperty(item))
-		.map(item => getNodeData(item, sharedInfo.expectedOrder));
-
 	// First, check order
-	allPropData.forEach(function checkEveryPropForOrder(propData, index) {
-		// return early if we know there is a violation and auto fix should be applied
-		if (shouldFixOrder && sharedInfo.isFixEnabled) {
-			return;
-		}
+	const allPropData = getAllPropData(node);
 
-		// current node should be a standard declaration
-		if (!utils.isProperty(propData.node)) {
-			return;
-		}
-
-		const previousPropData = allPropData[index - 1];
-
-		// Skip first decl
-		if (previousPropData) {
-			const checkedOrder = checkOrder({
-				firstPropData: previousPropData,
-				secondPropData: propData,
-				unspecified: sharedInfo.unspecified,
-				allPropData: allPropData.slice(0, index),
-			});
-
-			if (!checkedOrder.result) {
-				if (sharedInfo.isFixEnabled) {
-					shouldFixOrder = true;
-				} else {
-					const { orderData } = checkedOrder.secondNode;
-
-					stylelint.utils.report({
-						message: sharedInfo.messages.expected(
-							checkedOrder.secondNode.name,
-							checkedOrder.firstNode.name,
-							orderData && orderData.groupName
-						),
-						node: checkedOrder.secondNode.node,
-						result: sharedInfo.result,
-						ruleName: sharedInfo.ruleName,
-					});
-				}
-			}
-		}
-	});
-
-	if (shouldFixOrder && sharedInfo.isFixEnabled) {
-		const sortingOptions = {
-			'properties-order': createFlatOrder(sharedInfo.expectation),
-			'unspecified-properties-position':
-				sharedInfo.unspecified === 'ignore' ? 'bottom' : sharedInfo.unspecified,
-		};
-
-		// creating PostCSS Root node with current node as a child,
-		// so PostCSS Sorting can process it
-		const tempRoot = postcss.root({ nodes: [originalNode] });
-
-		postcssSorting(sortingOptions)(tempRoot);
+	if (!sharedInfo.isFixEnabled) {
+		allPropData.forEach(checkEveryPropForOrder);
 	}
 
+	if (sharedInfo.isFixEnabled) {
+		let shouldFixOrder = false;
+
+		allPropData.forEach(function checkEveryPropForOrder2(propData, index) {
+			// return early if we know there is a violation and auto fix should be applied
+			if (shouldFixOrder) {
+				return;
+			}
+
+			const previousPropData = allPropData[index - 1];
+
+			// Skip first decl
+			if (previousPropData) {
+				const checkedOrder = checkOrder({
+					firstPropData: previousPropData,
+					secondPropData: propData,
+					unspecified: sharedInfo.unspecified,
+					allPropData: allPropData.slice(0, index),
+				});
+
+				if (!checkedOrder.result) {
+					shouldFixOrder = true;
+				}
+			}
+		});
+
+		if (shouldFixOrder) {
+			const sortingOptions = {
+				'properties-order': createFlatOrder(sharedInfo.expectation),
+				'unspecified-properties-position':
+					sharedInfo.unspecified === 'ignore' ? 'bottom' : sharedInfo.unspecified,
+			};
+
+			// creating PostCSS Root node with current node as a child,
+			// so PostCSS Sorting can process it
+			const tempRoot = postcss.root({ nodes: [originalNode] });
+
+			postcssSorting(sortingOptions)(tempRoot);
+
+			const allPropData2 = getAllPropData(node);
+
+			allPropData2.forEach(checkEveryPropForOrder);
+		}
+	}
+
+	// Second, check emptyLineBefore
 	sharedInfo.lastKnownSeparatedGroup = 1;
 
 	const allNodesData = node.nodes.map(function collectDataForEveryNode(child) {
 		return getNodeData(child, sharedInfo.expectedOrder);
 	});
 
-	// Second, check emptyLineBefore
 	allNodesData.forEach(function checkEveryPropForEmptyLine(nodeData, index) {
 		let previousNodeData = allNodesData[index - 1];
 
@@ -103,4 +91,39 @@ module.exports = function checkNode(node, sharedInfo, originalNode) {
 
 		checkEmptyLineBefore(previousNodeData, nodeData, sharedInfo);
 	});
+
+	function checkEveryPropForOrder(propData, index, listOfProps) {
+		const previousPropData = listOfProps[index - 1];
+
+		// Skip first decl
+		if (previousPropData) {
+			const checkedOrder = checkOrder({
+				firstPropData: previousPropData,
+				secondPropData: propData,
+				unspecified: sharedInfo.unspecified,
+				allPropData: listOfProps.slice(0, index),
+			});
+
+			if (!checkedOrder.result) {
+				const { orderData } = checkedOrder.secondNode;
+
+				stylelint.utils.report({
+					message: sharedInfo.messages.expected(
+						checkedOrder.secondNode.name,
+						checkedOrder.firstNode.name,
+						orderData && orderData.groupName
+					),
+					node: checkedOrder.secondNode.node,
+					result: sharedInfo.result,
+					ruleName: sharedInfo.ruleName,
+				});
+			}
+		}
+	}
+
+	function getAllPropData(inputNode) {
+		return inputNode.nodes
+			.filter(item => utils.isProperty(item))
+			.map(item => getNodeData(item, sharedInfo.expectedOrder));
+	}
 };
---
+++ rules/properties-order/tests/report-when-not-fixed.js
@@ -0,0 +1,79 @@
+const stylelint = require('stylelint');
+const { ruleName } = require('../');
+
+test(`show warning if --fix enabled, but it didn't fix`, () => {
+	const code = `
+		const Component = styled.div\`
+			color: tomato;
+			\${interpolation}
+			top: 0;
+		\`;
+	`;
+
+	const stylelintConfig = {
+		plugins: ['./'],
+		rules: {
+			[ruleName]: [['top', 'color']],
+		},
+	};
+
+	const options = {
+		code,
+		config: stylelintConfig,
+		syntax: 'css-in-js',
+		fix: true,
+	};
+
+	return stylelint.lint(options).then(output => {
+		expect(output.results[0].warnings.length).toBe(1);
+
+		const fixedCode = getOutputCss(output);
+
+		expect(fixedCode).toBe(code);
+	});
+});
+
+test(`show warning if --fix enabled, and it fixed`, () => {
+	const code = `
+		const Component = styled.div\`
+			color: tomato;
+			top: 0;
+		\`;
+	`;
+
+	const expectedCode = `
+		const Component = styled.div\`
+			top: 0;
+			color: tomato;
+		\`;
+	`;
+
+	const stylelintConfig = {
+		plugins: ['./'],
+		rules: {
+			[ruleName]: [['top', 'color']],
+		},
+	};
+
+	const options = {
+		code,
+		config: stylelintConfig,
+		syntax: 'css-in-js',
+		fix: true,
+	};
+
+	return stylelint.lint(options).then(output => {
+		expect(output.results[0].warnings.length).toBe(0);
+
+		const fixedCode = getOutputCss(output);
+
+		expect(fixedCode).toBe(expectedCode);
+	});
+});
+
+function getOutputCss(output) {
+	const result = output.results[0]._postcssResult;
+	const css = result.root.toString(result.opts.syntax);
+
+	return css;
+}
