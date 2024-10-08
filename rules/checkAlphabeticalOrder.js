46a70cc159a1ea5f84b052fe6de75fd8fd29afa3
Szymon Małolepszy <szymon.malolepszy@softwareplant.com>
2024-10-08 09:15:13 +0200

    [WIP] First attempt

--- rules/checkAlphabeticalOrder.js
+++ rules/checkAlphabeticalOrder.js
@@ -7,29 +7,32 @@ function isShorthand(a, b) {
 	return longhands.includes(b);
 }
 
+function hasPrefix(propName) {
+	return vendor.prefix(propName).length > 0;
+}
+
 export function checkAlphabeticalOrder(firstPropData, secondPropData) {
+	const firstUnprefixedNameLC = firstPropData.unprefixedName.toLowerCase();
+	const secondUnprefixedNameLC = secondPropData.unprefixedName.toLowerCase();
+
 	// OK if the first is shorthand for the second:
-	if (isShorthand(firstPropData.unprefixedName, secondPropData.unprefixedName)) {
+	if (isShorthand(firstUnprefixedNameLC, secondUnprefixedNameLC)) {
 		return true;
 	}
 
 	// Not OK if the second is shorthand for the first:
-	if (isShorthand(secondPropData.unprefixedName, firstPropData.unprefixedName)) {
+	if (isShorthand(firstUnprefixedNameLC, secondUnprefixedNameLC)) {
 		return false;
 	}
 
 	// If unprefixed prop names are the same, compare the prefixed versions
-	if (firstPropData.unprefixedName === secondPropData.unprefixedName) {
-		// If first property has no prefix and second property has prefix
-		if (
-			!vendor.prefix(firstPropData.name).length &&
-			vendor.prefix(secondPropData.name).length
-		) {
+	if (firstUnprefixedNameLC === secondUnprefixedNameLC) {
+		if (!hasPrefix(firstPropData.name) && hasPrefix(secondPropData.name)) {
 			return false;
 		}
 
 		return true;
 	}
 
-	return firstPropData.unprefixedName < secondPropData.unprefixedName;
+	return firstUnprefixedNameLC < secondUnprefixedNameLC;
 }
